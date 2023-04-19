5b0bd3015059dc56a6c0a0e441b30dc10112e7b8
aishirin <awrnjoomartova@gmail.com>
2023-04-19 12:40:43 +0600

    MOtion div added

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -47,9 +47,24 @@ const Hero = () => {
         <DeskCanvas/>
         
       
-      <div className="absolute xs:bottom-10 bottom-32 w-full flex justify-center items-center">
-      
-               
+      <div className="hidden absolute  bottom-7 w-full sm:flex justify-end items-end">
+      <div className='hidden absolute  bottom-7 w-full sm:flex justify-end items-end'>
+        <a href='#about'>
+          <div className='w-[35px] h-[64px] rounded-3xl border-4 border-secondary flex justify-center items-start p-2'>
+            <motion.div
+              animate={{
+                y: [0, 24, 0],
+              }}
+              transition={{
+                duration: 1.5,
+                repeat: Infinity,
+                repeatType: "loop",
+              }}
+              className='w-3 h-3 rounded-full bg-secondary mb-1'
+            />
+          </div>
+        </a>
+      </div>
       </div>
 
      
